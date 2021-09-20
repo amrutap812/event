fa1a3387703b5484d0fcfa0cee070ad184e71555
apatil01 <amrutapatil812@gmail.com>
2021-09-20 12:40:26 +0530

    demo

--- app/controllers/user_shedules_controller.rb
+++ app/controllers/user_shedules_controller.rb
@@ -19,7 +19,7 @@ class UserShedulesController < ApplicationController
 
 				@user_shedule = UserShedule.where(user_id: current_user.id, shedule_id: event.id).first
 				
-				user_shedule.destroy
+				@user_shedule.destroy
 				flash[:notice] = "#{event.event_name} was successfully cancelled."
 				redirect_to booked_event_path
              
